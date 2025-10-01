8b885eb368f97dd45840e80ad167f32fa90a66c3
Kai'den Engle <kaidenengle8@gmail.com>
2025-10-01 19:56:08 -0400

    cleanup

--- dependencies/primitives/vertex.h
+++ dependencies/primitives/vertex.h
@@ -1,7 +1,8 @@
 #pragma once
+#include <glm/glm/glm.hpp>
 
 struct Vertex{
         glm::vec3 position;
         glm::vec3 color;
         float s, t;
-    };
+};
--- src/main.cpp
+++ src/main.cpp
@@ -19,14 +19,14 @@ const unsigned int SCR_HEIGHT = 600;
 int main()
 {
     // glfw: initialize and configure
-    // ------------------------------
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // glfw window creation
-    // --------------------
+
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -38,17 +38,18 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // glad: load all OpenGL function pointers
-    // ---------------------------------------
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
     
-    // build and compile our shader program
-    // ------------------------------------
+    // build and compile shader program
+
     Shader ourShader("C:/Users/Kaiden Engle/code/OpenGL-Tri/src/3.3.shader.vs", "C:/Users/Kaiden Engle/code/OpenGL-Tri/src/3.3.shader.fs");
 
+    // define vertex struct for reuseability
 
     struct Vertex{
         glm::vec3 position;
@@ -57,22 +58,34 @@ int main()
     };
     
     // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
+
     Vertex vertices[] = {
-        // positions        // colors          // texture coords    
-        glm::vec3(0.5f, -0.5f, 0.0f),  
+        
+        // top right
+        glm::vec3(0.5f, 0.5f, 0.0f),  
+        glm::vec3(1.0f, 0.0f, 0.0f),  
+        1.0f, 1.0f,
+    // bottom right
+        glm::vec3(0.5f,  -0.5f, 0.0f),  
         glm::vec3(0.0f, 1.0f, 0.0f),  
-        1.0f, 0.0f,  // bottom right
+        1.0f, 0.0f,
+        // bottom left
         glm::vec3(-0.5f, -0.5f, 0.0f),  
         glm::vec3(0.0f, 0.0f, 1.0f),  
-        0.0f, 0.0f,  // bottom left
-        glm::vec3(0.0f,  0.5f, 0.0f),  
+        0.0f, 0.0f,
+        
+        // top left
+        glm::vec3(-0.5f, 0.5f, 0.0f),  
         glm::vec3(1.0f, 1.0f, 0.0f),  
-        0.0f, 1.0f   // top
+        0.0f, 1.0f,
+        
+        
+        
     };
 
     unsigned int indices[] = {
         0, 1, 3,  // first triangle
+        1, 2, 3   // second triangle
     };
     
     
@@ -82,6 +95,7 @@ int main()
     glGenBuffers(1, &EBO);
 
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -91,16 +105,22 @@ int main()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position attribute
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    
     // color attribute
+    
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    
     // texture coord attribute
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
     // load and create texture
+   
     GLuint texture1, texture2;
 
     // texture 1
@@ -108,18 +128,22 @@ int main()
     glGenTextures(1, &texture1);
 
     // bind textures on corresponding texture units
+    
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture1);
     
-    // Setting wrap method (on current bound texture object)
+    // setting wrap method (on current bound texture object)
+    
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     
     // Setting filtering method (on current bound texture object)
+    
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     
     // load image, create texture, and generate mipmaps
+   
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true);
     unsigned char *data = stbi_load("C:/Users/Kaiden Engle/code/OpenGL-Tri/resources/textures/goon.png", &width, &height, &nrChannels, 0);
@@ -142,19 +166,23 @@ int main()
     glGenTextures(1, &texture2);
     
     // bind textures on corresponding texture units
+    
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, texture2);
     
     // Setting wrap method (on current bound texture object)
+    
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     
     // Setting filtering method (on current bound texture object)
+    
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     
     // load image, create texture, and generate mipmaps
     //stbi_set_flip_vertically_on_load(true);
+    
     data = stbi_load("C:/Users/Kaiden Engle/code/OpenGL-Tri/resources/textures/TerryDavis.png", &width, &height, &nrChannels, 0);
     if(data){
         if(nrChannels == 4){
@@ -174,27 +202,27 @@ int main()
     ourShader.setInt("texture2", 1);
 
     // render loop
-    // -----------
+    
     while (!glfwWindowShouldClose(window))
     {
 
         // input
-        // -----
+
         processInput(window);
 
-
         // render
-        // ------
+       
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // render the container
+
         ourShader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -209,13 +237,13 @@ int main()
     glDeleteTextures(1, &texture2);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
+
     glfwTerminate();
     return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
+
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -223,10 +251,11 @@ void processInput(GLFWwindow *window)
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
+    // height will be significantly larger than specified on retina displays
+
     glViewport(0, 0, width, height);
 }
